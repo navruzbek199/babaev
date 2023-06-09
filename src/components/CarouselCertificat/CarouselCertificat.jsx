b1b970b06eb66c506b4a5305e011efb81332b70d
MuhammadqodirProgrammer <komilovm831@gmail.com>
2023-06-09 16:58:06 +0500

    builded

--- src/components/CarouselCertificat/CarouselCertificat.jsx
+++ src/components/CarouselCertificat/CarouselCertificat.jsx
@@ -33,27 +33,10 @@ export default function CarouselCertificat() {
 			<div className='project_slide'>
 				<Swiper
 					modules={[Pagination]}
-					spaceBetween={50}
+					spaceBetween={10}
 					slidesPerView={1}
 					pagination={{ clickable: true }}
-					breakpoints={{
-						'@0.00': {
-							slidesPerView: 1,
-							spaceBetween: 10,
-						},
-						'@0.75': {
-							slidesPerView: 2,
-							spaceBetween: 20,
-						},
-						'@1.00': {
-							slidesPerView: 3,
-							spaceBetween: 40,
-						},
-						'@1.50': {
-							slidesPerView: 4,
-							spaceBetween: 50,
-						},
-					}}
+			
 				>
 		<SwiperSlide>
 					<div className='serti_card'>
